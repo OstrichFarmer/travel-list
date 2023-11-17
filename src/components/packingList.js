a46717270565977654e78236eb0b4ccfc19916be
Joel Stephen <joel.stephen56.sj@gmail.com>
2023-11-17 14:14:59 +0100

    created the item component

--- src/components/packingList.js
+++ src/components/packingList.js
@@ -1,5 +1,16 @@
 import "../index.css";
+import Item from "./item";
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
 export default function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <ul className="list">
+      {initialItems.map((item) => (
+        <Item item={item} />
+      ))}
+    </ul>
+  );
 }
