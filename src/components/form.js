b7073dacad4a874369fd3ddb75a51e4686dced3f
Joel Stephen <joel.stephen56.sj@gmail.com>
2023-11-17 20:05:18 +0100

    used array function for the select component

--- src/components/form.js
+++ src/components/form.js
@@ -1,8 +1,17 @@
 import "../index.css";
 export default function Form() {
   return (
-    <div className="add-form">
+    <form className="add-form">
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input placeholder="item...." />
+      <button>ADD</button>
+    </form>
   );
 }
--- src/components/packingList.js
+++ src/components/packingList.js
@@ -4,15 +4,16 @@ import Item from "./item";
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 1, description: "charger", quantity: 1, packed: false },
-  { id: 2, description: "laptop", quantity: 1, packed: true },
+  { id: 3, description: "charger", quantity: 1, packed: false },
+  { id: 4, description: "laptop", quantity: 1, packed: true },
 ];
+
 export default function PackingList() {
   return (
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
