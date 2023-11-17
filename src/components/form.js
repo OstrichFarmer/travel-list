94712e74960a289ed164af2eff04db0467a5f0da
Joel Stephen <joel.stephen56.sj@gmail.com>
2023-11-17 11:19:21 +0100

    finished the Form component

--- src/components/form.js
+++ src/components/form.js
@@ -1,1 +1,8 @@
-export default function Form() {}
+import "../index.css";
+export default function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
