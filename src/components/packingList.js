57f3e7622f4908d4d074c73a26bf9d3bfb3a3272
Joel Stephen <joel.stephen56.sj@gmail.com>
2023-11-17 20:07:19 +0100

    fixes

--- src/components/packingList.js
+++ src/components/packingList.js
@@ -3,7 +3,7 @@ import Item from "./item";
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 2, description: "Socks", quantity: 10, packed: true },
   { id: 3, description: "charger", quantity: 1, packed: false },
   { id: 4, description: "laptop", quantity: 1, packed: true },
 ];
