6088e56b57ab9d8fbdcbe15571c9e5724aaa797b
Joel Stephen <joel.stephen56.sj@gmail.com>
2023-11-22 15:38:27 +0100

    used a conditional rendering

--- src/components/stats.js
+++ src/components/stats.js
@@ -1,6 +1,17 @@
 import "../index.css";
 
 export default function Stats({ items }) {
+  if (!items.length)
+    return (
+      <div className="stats">
+        <footer>
+          <em>
+            <p>Start adding some items to your packing list 🚀</p>
+          </em>
+        </footer>
+      </div>
+    );
+
   const numItems = items.length;
   const numPacked = items.filter((item) => item.packed).length;
   const percentage = Math.round((numPacked / numItems) * 100);
@@ -8,8 +19,10 @@ export default function Stats({ items }) {
     <div className="stats">
       <footer>
         <em>
-          🧳 You have {numItems} item on your list, you have already packed{" "}
-          {numPacked}({percentage}%)
+          {percentage === 100
+            ? "You got everything, Ready to go  ✈️"
+            : `🧳 You have ${numItems} item on your list, you have already packed
+          ${numPacked} (${percentage}%)`}
         </em>
       </footer>
     </div>
