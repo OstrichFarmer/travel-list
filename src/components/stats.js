e8cffcc8cf35b32dbe13b9d04ff48ef8d6bfa8b0
Joel Stephen <joel.stephen56.sj@gmail.com>
2023-11-22 15:37:42 +0100

    used derived state to get the item length and packed item

--- src/components/stats.js
+++ src/components/stats.js
@@ -1,10 +1,16 @@
 import "../index.css";
 
-export default function Stats() {
+export default function Stats({ items }) {
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
   return (
     <div className="stats">
       <footer>
-        <em>🧳 You have X item on your list, you have already packed X(X%)</em>
+        <em>
+          🧳 You have {numItems} item on your list, you have already packed{" "}
+          {numPacked}({percentage}%)
+        </em>
       </footer>
     </div>
   );
